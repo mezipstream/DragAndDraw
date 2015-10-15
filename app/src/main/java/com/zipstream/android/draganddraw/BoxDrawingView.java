4984bff6c63725334646c0966f9f5c592006bba5
Eric <mezipstream@Outlook.com>
2015-10-15 09:34:07 +0800

    Initial commit

---
+++ app/src/main/java/com/zipstream/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,18 @@
+package com.zipstream.android.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+
+    //Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        super(context);
+    }
+
+    //Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
